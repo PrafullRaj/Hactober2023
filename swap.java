874e99f81dda33957edd5db766380b33b1e3acde
Raushan <91674489+Raushan-Kumar-Sinha@users.noreply.github.com>
2023-10-04 09:01:15 +0530

    Create swap.java
    Java program to swap two numbers

---
+++ swap.java
@@ -0,0 +1,19 @@
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 12.0f, second = 24.5f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        first = first - second;
+        second = first + second;
+        first = second - first;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
